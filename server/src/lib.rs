57400326ea9d54cea9dcfec781c083b95b41892b
Julian Beier <jb@futureplay.de>
2022-10-22 00:45:38 +0200

    parse test data correctly

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,11 +1,12 @@
 use std::net::{UdpSocket, SocketAddr};
 
-use packets::request::CommandPacket;
+use packets::request::Request;
+use packets::reply::Reply;
 
 static mut CTR: u32 = 0;
 
 pub fn handle_request(buf: &[u8], _socket: &UdpSocket, _conn: SocketAddr) {
-    let packet = CommandPacket::from(Vec::from(buf));
+    let packet = Request::from(Vec::from(buf));
     println!("{} {:?}", unsafe { CTR }, packet);
     unsafe { CTR += 1 };
 }
@@ -14,7 +15,10 @@ pub fn parse_packets() {
     let data = std::fs::read("./foo.conv").unwrap();
     let lines: Vec<&[u8]> = data.split(|x| *x == 0x15).filter(|x| x.len() > 0).collect();
     for p in lines {
-        let packet = CommandPacket::from(Vec::from(p));
-        println!("{:?}", packet);
+        if p[0] == 0 {
+            println!("cli => srv: {:?}", Request::from(Vec::from(&p[1..])));
+        } else {
+            println!("srv => cli: {:?}", Reply::from(Vec::from(&p[1..])));
+        }
     }
 }
